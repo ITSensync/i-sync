cf0bc9c77924b87ba01be389dd2c36b8f170c27d
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2025-02-18 11:16:37 +0700

    feat: stuff service

--- frontend/app/data/services/index.ts
+++ frontend/app/data/services/index.ts
@@ -1,3 +1,5 @@
 import { BoxService } from "./BoxService";
+import { StuffService } from "./StuffService";
 
-export const boxService = new BoxService(`${process.env.API_URL}/boxes/`);
+export const boxService = new BoxService(`${process.env.API_URL}/boxes`);
+export const stuffService = new StuffService(`${process.env.API_URL}/stuff`)
