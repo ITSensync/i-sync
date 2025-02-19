7bea94d22e368edd8d94ba3c0838dd6edcc1e5c2
alief abdillah <aliefmabdillah09@gmail.com>
2025-02-19 20:41:40 +0700

    refactor: font size

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <div className="flex justify-center md:h-20 h-16 mt-4 inset-x-0 bottom-0 w-full bg-blue-500 text-white p-4 items-center">
       <div className="flex md:flex-row flex-col items-center">
-        <p className="text-center lg:text-lg md:text-md sm:text-sm text-xs">
+        <p className="text-center md:text-md sm:text-sm text-xs">
           &copy; 2025 PT Sensor Teknologi Indonesia (Sensync).
         </p>
-        <p className="lg:text-lg md:text-md sm:text-sm text-xs">All rights reserved.</p>
+        <p className="md:text-md sm:text-sm text-xs md:ml-1">All rights reserved.</p>
       </div>
     </div>
   );
