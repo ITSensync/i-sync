73477d13ce17d797c0323625388ab64ae5b930bd
alief abdillah <aliefmabdillah09@gmail.com>
2025-02-21 17:25:47 +0700

    refactor: size div container

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="flex justify-center md:h-20 h-16 mt-4 inset-x-0 bottom-0 w-full bg-blue-500 text-white p-4 items-center">
+    <div className="flex justify-center h-12 mt-4 inset-x-0 bottom-0 w-full bg-blue-400 text-white p-4 items-center">
       <div className="flex md:flex-row flex-col items-center">
         <p className="text-center md:text-md sm:text-sm text-xs">
           &copy; 2025 PT Sensor Teknologi Indonesia (Sensync).
