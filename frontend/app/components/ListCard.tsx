ae2a7fa061929217731100bc380c56777117163c
alief abdillah <aliefmabdillah09@gmail.com>
2025-02-21 17:26:41 +0700

    feat: search barang

--- frontend/app/components/ListCard.tsx
+++ frontend/app/components/ListCard.tsx
@@ -1,23 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import React, { useEffect, useState } from "react";
-import AddIcon from "@mui/icons-material/Add";
 import Link from "next/link";
 import FormBox from "./FormBox";
 import { boxService } from "../data/services";
-import { Box } from "../types/Box";
+import { Box, BoxDetail } from "../types/Box";
 import { ApiError } from "../types/ApiError";
 import Loading from "./Loader/Loading";
 import EmptyPage from "./EmptyPage";
 import ErrorToast from "./ErrorToast";
 import Image from "next/image";
 import DeleteModal from "./DeleteModal";
+import SearchResult from "./SearchResult";
 
 export default function ListCard() {
   const [isFormShow, setIsFormShow] = useState(false);
   const [editedBox, setEditedBox] = useState<any>();
   const [deletedBox, setDeletedBox] = useState<any>();
-  const [boxesData, setBoxesData] = useState<Box[]>([]);
+  const [boxesData, setBoxesData] = useState<BoxDetail[]>([]);
+  const [filteredBoxes, setFilteredBoxes] = useState<BoxDetail[]>([]);
+  const [inputSearch, setInputSearch] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isToastOpen, setIsToastOpen] = useState(false);
   const [errorData, setErrorData] = useState<ApiError>({
@@ -37,7 +39,7 @@ export default function ListCard() {
     setIsLoading(true);
     const response = await boxService.getListBox();
     if (response.data) {
-      const boxResult: Box[] = response.data;
+      const boxResult: BoxDetail[] = response.data;
       setBoxesData(boxResult);
     } else {
       setErrorData({
@@ -72,8 +74,32 @@ export default function ListCard() {
     setBoxesData((prevData) => prevData.filter((item) => item.id !== id));
   };
 
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const searchText = event.target.value.toLowerCase();
+    setInputSearch(searchText);
+
+    if (!searchText) {
+      setFilteredBoxes([]);
+    } else {
+      const filtered = boxesData
+        .map((box) => {
+          // Filter hanya stuff yang cocok
+          const matchingStuff = box.stuff.filter((s) =>
+            s.name.toLowerCase().includes(searchText)
+          );
+
+          return matchingStuff.length > 0
+            ? { ...box, stuff: matchingStuff }
+            : null;
+        })
+        .filter(Boolean) as BoxDetail[];
+
+      setFilteredBoxes(filtered);
+    }
+  };
+
   return (
-    <div className="w-[100vw] sm:w-full mb-8">
+    <div className="w-[100vw] sm:w-full mb-8 mt-4">
       <ErrorToast
         isOpen={isToastOpen}
         error={errorData}
@@ -88,17 +114,46 @@ export default function ListCard() {
         />
       ) : (
         <>
-          <div className="flex justify-end px-4 lg:w-4/5 md:w-full w-full">
-            <button
-              onClick={() => handleBtnAddState(true)}
-              className="btn btn-success btn-sm sm:btn-md text-white"
-            >
-              <AddIcon className="md:inline !hidden" style={{ fontSize: 35 }} />
-              <p className="md:hidden !inline">+</p>
-              <p className="font-poppins text-sm sm:text-lg">Add</p>
-            </button>
+          <div className="flex justify-center">
+            <div className="flex flex-row justify-between items-center w-full max-w-4xl px-4">
+              <label
+                className="input bg-white input-bordered input-sm sm:input-md border-zinc-300 
+                flex items-center gap-0 sm:gap-2 shadow-lg"
+              >
+                <input
+                  type="text"
+                  className="text-sm sm:text-base w-28 sm:w-48 bg-red-400"
+                  placeholder="Cari barang"
+                  onChange={handleSearch}
+                  value={inputSearch}
+                />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 16 16"
+                  fill="currentColor"
+                  className="w-4 h-4 sm:h-6 sm:w-6 opacity-70 text-blue-400"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </label>
+              <button
+                onClick={() => handleBtnAddState(true)}
+                className="btn btn-success btn-sm sm:btn-md text-white"
+              >
+                <p className="inline-block text-xl">+</p>
+                <p className="font-poppins text-sm sm:text-lg hidden sm:inline">
+                  Tambah
+                </p>
+              </button>
+            </div>
           </div>
-          {isLoading ? (
+          {inputSearch ? (
+            <SearchResult searchResult={filteredBoxes} />
+          ) : isLoading ? (
             <div className="flex justify-center items-center p-8">
               <Loading />
             </div>
